6edd1d81ac3fb223b1ecfa2a845058e829ffefb9
ziad el-hefnawy <67851652+ze-h@users.noreply.github.com>
2023-10-09 01:26:46 +0000

    added vpic example

--- lib/fetch.py
+++ lib/fetch.py
@@ -2,7 +2,7 @@ import json
 import requests
 from vehicle import Vehicle
 
-url = "https://sample.com/"
+url = "https://vpic.nhtsa.dot.gov/api/"
 
 def get_car(v: Vehicle) -> list:
     requests.get(url=(url + ""))
--- lib/sample.py
+++ lib/sample.py
@@ -1,2 +1,12 @@
-def test() -> str:
-    return "Hello, World!"
+import requests
+
+URL = "https://vpic.nhtsa.dot.gov/api/"
+
+# Returns the number of cylinders in a 2013 BMW X3
+def x3() -> str:
+    arg = "vehicles/decodevin/5UXWX7C5*BA?format=json&modelyear=2011"
+    try:
+        response = requests.get(url=(URL + arg), timeout=30)
+    except(requests.Timeout):
+        return None
+    return response.json().get("Results")[70].get("Value")
